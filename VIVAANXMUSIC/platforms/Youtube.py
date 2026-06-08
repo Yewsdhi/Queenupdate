01e60b6a7d93d512405aa5dee5c1bf72c7c8a48c
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2026-06-08 18:13:45 +0530

    Update Youtube.py

--- VIVAANXMUSIC/platforms/Youtube.py
+++ VIVAANXMUSIC/platforms/Youtube.py
@@ -5,7 +5,6 @@ from typing import Union
 import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
-from py_yt import VideosSearch
 from youtubesearchpython import VideosSearch
 import aiohttp
 
